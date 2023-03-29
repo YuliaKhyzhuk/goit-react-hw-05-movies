cdd01bf567c86187d35ff58cde1efe7e87b72536
YuliaKhyzhuk <anastezy1@gmail.com>
2023-03-30 01:16:32 +0300

    MovieList added

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,7 +1,30 @@
-import React from "react";
-import { Suspense } from 'react';
-import { Outlet } from "react-router-dom";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const MovieList = () => {}
+import { Link, useLocation } from 'react-router-dom';
+
+const MovieList = ({ movies, title }) => {
+  const location = useLocation();
+
+  return (
+    <div>
+      {title && <h2>{title}</h2>}
+      <ul>
+        {movies?.length &&
+          movies.map(movie => {
+           return (<li key={movie.id}>
+            <Link to={`/movies/${movie.id}`} state={{ from: location }}>
+              {movie.title}
+            </Link>
+          </li>); 
+          })}
+      </ul>
+    </div>
+  );
+};
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({id: PropTypes.number.isRequired})).isRequired,
+};
 
 export default MovieList;

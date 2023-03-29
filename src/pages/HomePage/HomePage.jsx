45c65e3b2e27c4d114296ea91cc5117658b02d86
YuliaKhyzhuk <anastezy1@gmail.com>
2023-03-30 00:44:45 +0300

    HomePage added

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,37 @@
-import React from "react";
-import { Suspense } from 'react';
-import { Outlet } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { getTrendingMovies } from '../../services/themoviedbApi';
 
-const HomePage = () => {}
+import { MovieList } from 'components/MovieList/MovieList';
+import { Loader } from 'components/Loader/Loader';
+
+const HomePage = () => {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getTrendingMoviesFromApi = async () => {
+      try {
+        setIsLoading(true);
+        const response = await getTrendingMovies();
+        setTrendingMovies(response.results);
+      } catch (error) {
+        setError(error.message);
+        console.log('getTrendingMovies error:', error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getTrendingMoviesFromApi();
+  }, []);
+
+  return (
+    <div>
+      {isLoading && <Loader />}
+      {error !== 0 && <p>Something went wrong! Please try again!</p>}
+      <MovieList movies={trendingMovies} title={'Trending today'} />
+    </div>
+  );
+};
 
 export default HomePage;
